664109d11f86d6a85edc11f39c5dd1c29e632889
mila <l.milaropot@gmail.com>
2023-11-03 17:39:14 +0300

    Add function for task2

--- task_two/open_file.py
+++ task_two/open_file.py
@@ -1,7 +1,22 @@
 import json
 
+from task_two.role_model import Role
+
 
 def open_file():
     with open("example.json", "r") as my_file:
         example_json = my_file.read()
         return json.loads(example_json)
+
+
+def role_list():
+    valuesList = []
+    dict_file = open_file()
+    for item in dict_file["data"]["items"]:
+        valuesList.append(Role(name=item['name'], id=item['id'], description=item['description']))
+    return valuesList
+
+# как из словаря вытащить список айтемс по ключу
+#внутри аппенд я должна добавлять обьект класса Role с заполнеными атрибутами из словарей внутри списка ^ из 1го
+#пункта
+
--- task_two/test_open_file.py
+++ task_two/test_open_file.py
@@ -1,4 +1,5 @@
-from task_two.open_file import open_file
+from task_two.open_file import open_file, role_list
+from task_two.role_model import Role
 
 
 def test():
@@ -26,3 +27,17 @@ def test():
     }
 
 
+def test_role_list():
+    assert role_list() == [
+        Role(name='name 1', id=123456, description='Описание 1'),
+        Role(name='name 2', id=654321, description='Описание 2'),
+        Role(name='name 3', id=987654, description='Описание 3')
+    ]
+
+
+def test_role_list2():
+    dict_file = open_file()
+    items = dict_file["data"]["items"]
+    data = dict_file.get("data")
+    items = data.get("items")
+    print(items[1].get("name"))
